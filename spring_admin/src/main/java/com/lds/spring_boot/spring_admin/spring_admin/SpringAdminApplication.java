a5085d5c1c7cdaf93fb446fefa276507a3678983
yumochen <1600611326@qq.com>
2020-03-02 10:21:19 +0800

    首次提交，测试用

---
+++ spring_admin/src/main/java/com/lds/spring_boot/spring_admin/spring_admin/SpringAdminApplication.java
@@ -0,0 +1,13 @@
+package com.lds.spring_boot.spring_admin.spring_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAdminApplication.class, args);
+	}
+
+}
---
+++ spring_admin/src/test/java/com/lds/spring_boot/spring_admin/spring_admin/SpringAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lds.spring_boot.spring_admin.spring_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
